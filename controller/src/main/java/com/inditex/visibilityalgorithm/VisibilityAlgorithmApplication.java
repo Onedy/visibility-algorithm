bbd392bc23f29abcfc62f41c20fa3ac8930a7d47
Eduard <eduard.onofrei@servexternos.gruposantander.com>
2022-12-05 02:01:37 +0100

    Project structure

---
+++ controller/src/main/java/com/inditex/visibilityalgorithm/VisibilityAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.visibilityalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisibilityAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisibilityAlgorithmApplication.class, args);
+	}
+
+}
